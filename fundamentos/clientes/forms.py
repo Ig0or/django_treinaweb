45fc326788f1a3ae85424d8c220bd91a5dbb9d73
Ig0or <igorprovensi@gmail.com>
2021-11-03 15:46:48 -0300

    models 1:1 cliente e endereco

--- fundamentos/clientes/forms.py
+++ fundamentos/clientes/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Cliente
+from django.db import models
+from django.forms import fields
+from .models import Cliente, Endereco
 
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ['nome', 'sexo', 'data_nascimento', 'email', 'profissao']
+
+class EnderecoForm(forms.ModelForm):
+    class Meta:
+        model = Endereco
+        fields = ['rua', 'numero', 'complemento', 'bairro', 'cidade', 'pais']
--- fundamentos/clientes/services/cliente_service.py
+++ fundamentos/clientes/services/cliente_service.py
@@ -16,8 +16,8 @@ def remover_cliente(cliente):
 
 
 def cadastrar_cliente(cliente):
-    Cliente.objects.create(nome=cliente.nome, sexo=cliente.sexo, data_nascimento=cliente.data_nascimento,
-                           email=cliente.email, profissao=cliente.profissao)
+    Cliente.objects.create(nome=cliente.nome, sexo=cliente.sexo, data_nascimento=cliente.data_nascimento, email=cliente.email, profissao=cliente.profissao,
+    endereco=cliente.endereco)
 
 
 def editar_cliente(cliente, cliente_novo):
@@ -26,4 +26,5 @@ def editar_cliente(cliente, cliente_novo):
     cliente.data_nascimento = cliente_novo.data_nascimento
     cliente.email = cliente_novo.email
     cliente.profissao = cliente_novo.profissao
+    cliente.endereco = cliente_novo.endereco
     cliente.save(force_update=True)
