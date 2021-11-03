998364b1fb7d24ce46e53302e0f0d5931608d7a4
Ig0or <igorprovensi@gmail.com>
2021-11-03 10:43:17 -0300

    criado models endereco - fundamentos

--- fundamentos/clientes/models.py
+++ fundamentos/clientes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.expressions import F
 
 # Create your models here.
 
@@ -18,3 +19,14 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Endereco(models.Model):
+    rua = models.CharField(max_length=200, null=False, blank=False)
+    numero = models.IntegerField(null=False, blank=False)
+    complemento = models.CharField(max_length=200, null=False, blank=False)
+    bairro = models.CharField(max_length=50, null=False, blank=False)
+    cidade = models.CharField(max_length=50, null=False, blank=False)
+    pais = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.rua
